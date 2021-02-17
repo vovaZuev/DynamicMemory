cff473298088e5208b3449db6ea9dc624176a103
vovaZuev <vovazuev@yandex.ru>
2021-02-17 23:17:29 +0700

    int* to void DONE

--- main.cpp
+++ main.cpp
@@ -3,22 +3,28 @@ using namespace std;
 
 void FillRand(int* arr, const int n);
 void Print(int* arr, const int n);
-
+/*--------------------------------------------------------
 int* Push_Back(int* arr, const int n, int push_back);
 int* Push_Front(int* arr, const int n, int push_front);
 int* Insert(int* arr, const int n, int insert, const int insert_index);
 int* Pop_Back(int* arr, const int n);
 int* Pop_Front(int* arr, const int n);
 int* Erase(int* arr, const int n, const int erase_index);
-
+-----------------------------------------------------------*/
+void PushBack(int** arr, int* n, int value);
+void PushFront(int** arr, int* n, int value);
+void Insert(int** arr, int* n, int value, int index);
+void PopBack(int** arr, int* n);
+void PopFront(int** arr, int* n);
+void Erase(int** arr, int* n, int index);
 
 int main()
 {
 	setlocale(LC_ALL, "");
 	int n;
-	int push_back, push_front;
-	int insert, insert_index;
-	int erase_index;
+	int pb_value, pf_value;
+	int ins_value, ins_index;
+	int ers_index;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
 
@@ -26,36 +32,36 @@ int main()
 	cout << "Исходный массив: " << endl;
 	Print(arr, n);
 
-	// ??? push_back(???);	//Добавляет значение в конец динамического массива.
-	cout << "Введите число, чтобы добавить в конец массива: "; cin >> push_back;
-	arr = Push_Back(arr, n, push_back);
-	Print(arr, ++n);
-
-	// ? ? ? push_front(? ? ? );	//Добавляет значение в начало динамического массива.
-	cout << "Введите число, чтобы добавить в начало массива: "; cin >> push_front;
-	arr = Push_Front(arr, n, push_front);
-	Print(arr, ++n);
-
-	// ??? insert(???);	//Добавляет значение в массив по указанному индексу.
-	cout << "Введите число, которое нужно вставить в массив, и индекс вставки: "; cin >> insert >> insert_index;
-	arr = Insert(arr, n, insert, insert_index);
-	Print(arr, ++n);
-
-	// ??? pop_back(???);	//Удаляет последний элемент массива.
+	//Добавляет значение в конец динамического массива.
+	cout << "Введите число, чтобы добавить в конец массива: "; cin >> pb_value;
+	PushBack(&arr, &n, pb_value);
+	Print(arr, n);
+	
+	//Добавляет значение в начало динамического массива.
+	cout << "Введите число, чтобы добавить в начало массива: "; cin >> pf_value;
+	PushFront(&arr, &n, pf_value);
+	Print(arr, n);
+	
+	//Добавляет значение в массив по указанному индексу.
+	cout << "Введите число, которое нужно вставить в массив, и индекс вставки: "; cin >> ins_value >> ins_index;
+	Insert(&arr, &n, ins_value, ins_index);
+	Print(arr, n);
+	
+	//Удаляет последний элемент массива.
 	cout << "Последний элемент массива удалён: " << endl;
-	arr = Pop_Back(arr, n);
-	Print(arr, n--); // Выводим n элементов, чтобы увидеть мусор на месте последнего элемента.
-
-	// ??? pop_front(???);	//Удаляет нулевой элемент массива.
+	PopBack(&arr, &n);
+	Print(arr, n); // Выводим n элементов, чтобы увидеть мусор на месте последнего элемента.
+	
+	//Удаляет нулевой элемент массива.
 	cout << "Нулевой элемент массива удалён: " << endl;
-	arr = Pop_Front(arr, n);
-	Print(arr, --n);
-
-	// ??? erase(???);		//Удаляет значение из массива по указанному индексу.
-	cout << "Введите индекс, по которому нужно удалить элемент: "; cin >> erase_index;
-	arr = Erase(arr, n, erase_index);
-	Print(arr, --n);
-
+	PopFront(&arr, &n);
+	Print(arr, n);
+	
+	//Удаляет значение из массива по указанному индексу.
+	cout << "Введите индекс, по которому нужно удалить элемент: "; cin >> ers_index;
+	Erase(&arr, &n, ers_index);
+	Print(arr, n);
+	
 	delete[] arr;
 }
 
@@ -63,7 +69,6 @@ void FillRand(int* arr, const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		// * - оператор разыменования, возвращает значение по адресу.
 		*(arr + i) = rand() % 100;
 	}
 }
@@ -71,12 +76,94 @@ void Print(int* arr, const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		// [] - оператор индексирования, возвращает значение по индексу.
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
 }
-
+void PushBack(int** arr, int* n, int value)
+{
+	int* temp = new int[*n + 1];
+	for (int i = 0; i < *n; i++)
+	{
+		temp[i] = (*arr)[i];
+	}
+	temp[*n] = value;
+	++(*n);
+	delete[] * arr;
+	*arr = temp;
+}
+void PushFront(int** arr, int* n, int value)
+{
+	int* temp = new int[*n + 1];
+	temp[0] = value;
+	for (int i = 1; i < *n + 1; i++)
+	{
+		temp[i] = (*arr)[i - 1];
+	}
+	++(*n);
+	delete[] * arr;
+	*arr = temp;
+}
+void Insert(int** arr, int* n, int value, int index)
+{
+	if (index >= 0 && index < *n)
+	{
+		int* temp = new int[*n + 1];
+		for (int i = 0; i < index; i++)
+		{
+			temp[i] = (*arr)[i];
+		}
+		*(temp + index) = value;
+		for (int i = index + 1; i < *n + 1; i++)
+		{
+			temp[i] = (*arr)[i - 1];
+		}
+		++(*n);
+		delete[] * arr;
+		*arr = temp;
+	}
+}
+void PopBack(int** arr, int* n)
+{
+	int* temp = new int[*n - 1];
+	for (int i = 0; i < *n - 1; i++)
+	{
+		temp[i] = (*arr)[i];
+	}
+	--(*n);
+	delete[] * arr;
+	*arr = temp;
+}
+void PopFront(int** arr, int* n)
+{
+	int* temp = new int[*n - 1];
+	for (int i = 0; i < *n - 1; i++)
+	{
+		temp[i] = (*arr)[i + 1];
+	}
+	--(*n);
+	delete[] * arr;
+	*arr = temp;
+}
+void Erase(int** arr, int* n, int index)
+{
+	if (index >= 0 && index < *n)
+	{
+		int* temp = new int[*n - 1];
+		for (int i = 0; i < index; i++)
+		{
+			*(temp + i) = (*arr)[i];
+		}
+		for (int i = index + 1; i < *n; i++)
+		{
+			temp[i - 1] = (*arr)[i];
+		}
+		--(*n);
+		delete[] * arr;
+		*arr = temp;
+	}
+}
+/*----------------------------------------------------------
 int* Push_Back(int* arr, const int n, int push_back)
 {
 	int* temp = new int[n + 1];
@@ -152,3 +239,4 @@ int* Erase(int* arr, const int n, const int erase_index)
 	else
 		return arr;
 }
+---------------------------------------------------------------*/
